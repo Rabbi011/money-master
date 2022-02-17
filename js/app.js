a400b71ffc5aa30ecd882d34f253189c761b9d49
rabbi011 <rakibil15-13011@diu.edu.bd>
2022-02-17 16:57:35 +0600

    allmost done

--- js/app.js
+++ js/app.js
@@ -20,18 +20,62 @@ document.getElementById('calculate-button').addEventListener('click',function(){
     // const calculateExpensesvalue = parseFloat(calculateExpensesText.innerText);
 // total value
    const  calculateTotalExpensesvalue = expensesClothesInputValue + expensesRentRnputValue + expensesFoodInputValue;
+   console.log(calculateTotalExpensesvalue)
     calculateExpensesText.innerText = calculateTotalExpensesvalue;
+    // balance
 
    const totalBalanceText = document.getElementById('total-balance');
+   const balanceAmount = incomeUserInputValue - calculateTotalExpensesvalue ;
+   totalBalanceText.innerText = balanceAmount ;
 
-totalBalanceText.innerText = incomeUserInputValue - calculateTotalExpensesvalue ;
+
 
 
     // console.log(calculateTotalExpensesvalue)
 
     // clear the expenses input
-    expensesFoodInputText.value = '';
-    expensesRentRnputText.value = '';
-    expensesClothesInputText.value = '';
-    incomeUserInputText.value = '';
+    // expensesFoodInputText.value = '';
+    // expensesRentRnputText.value = '';
+    // expensesClothesInputText.value = '';
+    // incomeUserInputText.value = '';
 });
+
+document.getElementById('save-button').addEventListener('click',function(){
+    // console.log('clicking ');
+    // double korse
+    const incomeUserInputText = document.getElementById('income-input');
+    const incomeUserInputValue = parseFloat(incomeUserInputText.value);
+
+    const saveInputFieldText = document.getElementById('save-input-field');
+    const saveInputFieldValue =  parseFloat(saveInputFieldText.value);
+    // console.log(saveInputFieldValue)
+    const savingAmountText = document.getElementById('saving-amount');
+    // show saving amount
+   const saveAmount = incomeUserInputValue - (((100 - saveInputFieldValue) * incomeUserInputValue) / 100 ) ;
+   console.log(saveAmount)
+    savingAmountText.innerText = saveAmount;
+    // console.log(saveAmount)
+    // double
+    const remainingBalance = document.getElementById('remaining-balance');
+
+    // double2
+    const expensesFoodInputText = document.getElementById('expenses-food-input');
+    const expensesFoodInputValue = parseFloat(expensesFoodInputText.value);
+
+    const expensesRentRnputText = document.getElementById('expenses-rent-input');
+    const expensesRentRnputValue = parseFloat(expensesRentRnputText.value);
+
+    const expensesClothesInputText = document.getElementById('expenses-clothes-input');
+    const expensesClothesInputValue = parseFloat(expensesClothesInputText.value);
+    const  calculateTotalExpensesvalue = expensesClothesInputValue + expensesRentRnputValue + expensesFoodInputValue;
+    // console.log( calculateTotalExpensesvalue)
+
+    const totalBalanceText = document.getElementById('total-balance');
+   const balanceAmount = incomeUserInputValue - calculateTotalExpensesvalue ;
+//    console.log(balanceAmount)
+
+
+
+remainingBalance.innerText =(parseFloat(balanceAmount) - saveAmount) ;
+
+})
