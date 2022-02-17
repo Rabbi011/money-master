b33e1dd0e4b1cd9be7e3bb5ac2c1d6edca730eb4
rabbi011 <rakibil15-13011@diu.edu.bd>
2022-02-17 14:00:17 +0600

    income exp total exp and balance

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log(10);
+document.getElementById('calculate-button').addEventListener('click',function(){
+    // console.log('clicked');
+    const incomeUserInputText = document.getElementById('income-input');
+    const incomeUserInputValue = parseFloat(incomeUserInputText.value);
+    // console.log(incomeUserInputValue)
+
+    const expensesFoodInputText = document.getElementById('expenses-food-input');
+    const expensesFoodInputValue = parseFloat(expensesFoodInputText.value);
+    // console.log(expensesFoodInputValue)
+
+    
+    const expensesRentRnputText = document.getElementById('expenses-rent-input');
+    const expensesRentRnputValue = parseFloat(expensesRentRnputText.value);
+    // console.log(expensesRentRnputValue)
+
+    const expensesClothesInputText = document.getElementById('expenses-clothes-input');
+    const expensesClothesInputValue = parseFloat(expensesClothesInputText.value);
+    // console.log(expensesClothesInputValue)
+    const calculateExpensesText = document.getElementById('calculate-total-expenses');
+    // const calculateExpensesvalue = parseFloat(calculateExpensesText.innerText);
+// total value
+   const  calculateTotalExpensesvalue = expensesClothesInputValue + expensesRentRnputValue + expensesFoodInputValue;
+    calculateExpensesText.innerText = calculateTotalExpensesvalue;
+
+   const totalBalanceText = document.getElementById('total-balance');
+
+totalBalanceText.innerText = incomeUserInputValue - calculateTotalExpensesvalue ;
+
+
+    // console.log(calculateTotalExpensesvalue)
+
+    // clear the expenses input
+    expensesFoodInputText.value = '';
+    expensesRentRnputText.value = '';
+    expensesClothesInputText.value = '';
+    incomeUserInputText.value = '';
+});
