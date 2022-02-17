77c926c2afa5ab444375e09dc69c3837d4948fab
rabbi011 <rakibil15-13011@diu.edu.bd>
2022-02-17 20:26:58 +0600

    function done

--- js/app.js
+++ js/app.js
@@ -1,68 +1,74 @@
+function getInputValue(inputId){
+    
+    const userInputText = document.getElementById(inputId);
+    const userInputValue = parseFloat(userInputText.value);
+    if(userInputValue > 0){
+        return userInputValue;
+    }
+    else if(userInputValue < 0){
+
+    }
+   
+}
+
 document.getElementById('calculate-button').addEventListener('click',function(){
     
-    const incomeUserInputText = document.getElementById('income-input');
-    const incomeUserInputValue = parseFloat(incomeUserInputText.value);
-    
+    // input part
+    const incomeUserInputValue = getInputValue('income-input'); 
+    const expensesFoodInputValue = getInputValue('expenses-food-input');
+    const expensesRentRnputValue = getInputValue('expenses-rent-input');
+    const expensesClothesInputValue = getInputValue('expenses-clothes-input');
 
-    const expensesFoodInputText = document.getElementById('expenses-food-input');
-    const expensesFoodInputValue = parseFloat(expensesFoodInputText.value);
-    
-
-    
-    const expensesRentRnputText = document.getElementById('expenses-rent-input');
-    const expensesRentRnputValue = parseFloat(expensesRentRnputText.value);
-    
-    const expensesClothesInputText = document.getElementById('expenses-clothes-input');
-    const expensesClothesInputValue = parseFloat(expensesClothesInputText.value);
-    
-    const calculateExpensesText = document.getElementById('calculate-total-expenses');
-   
-// total value
+    // total value
    const  calculateTotalExpensesvalue = expensesClothesInputValue + expensesRentRnputValue + expensesFoodInputValue;
-   
+   if(incomeUserInputValue > calculateTotalExpensesvalue){
+    const calculateExpensesText = document.getElementById('calculate-total-expenses');
     calculateExpensesText.innerText = calculateTotalExpensesvalue;
-    // balance
-
-   const totalBalanceText = document.getElementById('total-balance');
+    const totalBalanceText = document.getElementById('total-balance');
    const balanceAmount = incomeUserInputValue - calculateTotalExpensesvalue ;
    totalBalanceText.innerText = balanceAmount ;
+   }
+   else{
+
+   }
+
  
 });
 
-document.getElementById('save-button').addEventListener('click',function(){
+     document.getElementById('save-button').addEventListener('click',function(){
    
-    // double korse
-    const incomeUserInputText = document.getElementById('income-input');
-    const incomeUserInputValue = parseFloat(incomeUserInputText.value);
+    const incomeUserInputValue = getInputValue('income-input'); 
+    
 // save part
     const saveInputFieldText = document.getElementById('save-input-field');
     const saveInputFieldValue =  parseFloat(saveInputFieldText.value);
     
     const savingAmountText = document.getElementById('saving-amount');
     // show saving amount
-   const saveAmount = incomeUserInputValue - (((100 - saveInputFieldValue) * incomeUserInputValue) / 100 ) ;
 
-  
-    savingAmountText.innerText = saveAmount;
+
+   const saveAmount = incomeUserInputValue - (((100 - saveInputFieldValue) * incomeUserInputValue) / 100 ) ;
     
-    // double
     const remainingBalance = document.getElementById('remaining-balance');
 
-    // double2
-    const expensesFoodInputText = document.getElementById('expenses-food-input');
-    const expensesFoodInputValue = parseFloat(expensesFoodInputText.value);
+    const expensesFoodInputValue = getInputValue('expenses-food-input');
+    const expensesRentRnputValue = getInputValue('expenses-rent-input');
+    const expensesClothesInputValue = getInputValue('expenses-clothes-input');
 
-    const expensesRentRnputText = document.getElementById('expenses-rent-input');
-    const expensesRentRnputValue = parseFloat(expensesRentRnputText.value);
-
-    const expensesClothesInputText = document.getElementById('expenses-clothes-input');
-    const expensesClothesInputValue = parseFloat(expensesClothesInputText.value);
     const  calculateTotalExpensesvalue = expensesClothesInputValue + expensesRentRnputValue + expensesFoodInputValue;
     
 
     const totalBalanceText = document.getElementById('total-balance');
    const balanceAmount = incomeUserInputValue - calculateTotalExpensesvalue ;
 
-remainingBalance.innerText =(parseFloat(balanceAmount) - saveAmount) ;
+  
+   if(incomeUserInputValue > calculateTotalExpensesvalue && balanceAmount > saveAmount){
+    savingAmountText.innerText = saveAmount;
+    remainingBalance.innerText =(parseFloat(balanceAmount) - saveAmount) ;
+    
+}
+else{
+    
+}
 
-})
+});
